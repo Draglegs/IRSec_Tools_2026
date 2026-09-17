9ccfd15a52356bfdfada7131f9b94eea67d89943
Jack Ford <jad2447@rit.edu>
2026-09-17 19:24:33 -0400

    first steps

--- Windows_Essential.ps1
+++ Windows_Essential.ps1
@@ -1,1 +1,14 @@
-#TODO: Add stuff
+<#This script disables autoruns, deletes bad users,
+and changes defualt passwords. It is meant to be run on all windows machines
+at the start of IRSec.#>
+
+#Removes global autoruns. User autoruns will persist after this step.
+#Remove-Item -Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\*"
+
+#Array of local user accounts
+$LocalUsers = Get-LocalUser
+echo "$LocalUsers"
+
+foreach ($User in $LocalUsers){
+    echo $User
+}
